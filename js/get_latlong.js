c8ddd59dabdeb81a6ea1ecb015915ce1185eae7f
Yuki_local <t12968yy@sfc.keio.ac.jp>
2012-12-20 18:34:27 +0900

    よくわからなくなってきたお

--- js/get_latlong.js
+++ js/get_latlong.js
@@ -1,5 +1,44 @@
 google.load("maps", "2.x");
 
+// 最寄り駅検索
+var zip_data = null;
+
+function search(post) {
+    var target = document.createElement('script');
+    target.charset = 'utf-8';
+    target.src = "http://geoapi.heartrails.com/api/json"
+        + "?method=getStations"
+        + "&postal=" + post
+        + "&jsonp=callbackf";
+    document.body.appendChild(target);
+}
+
+function callbackf(data) {
+    var out = document.getElementById('codes');
+    out.innerHTML += JSON.stringify(data);
+}
+
+// 郵便番号変換
+var zip_data = null;
+
+function postal(x,y) {
+    var target = document.createElement('script2');
+    target.charset = 'utf-8';
+    target.src = "http://geoapi.heartrails.com/api/json"
+        + "?method=searchByGeoLocation"
+        + "&x=" + x
+        + "&y=" + y
+        + "&jsonp=callbackf";
+    document.body.appendChild(target);
+}
+
+function callbackf(data) {
+    var out = document.getElementById('codes');
+    out.innerHTML += JSON.stringify(data);
+}
+
+
+// 現在地表示
 function initialize() {
     if (navigator.geolocation) {
         //Geolocation API を使った現在地の取得
@@ -10,6 +49,11 @@ function initialize() {
 //現在地の位置情報を表示する
 function showPosition(position) {
     document.getElementById("location").innerHTML = "(" + position.coords.latitude + ", " + position.coords.longitude + ")";
+
+// postalに変換するのが必要
+    document.getElementById("codes").innerHTML = search(position.coords.latitude, position.coords.longitude);
+
+
     showAddress(position.coords.latitude, position.coords.longitude);
 }
 
@@ -17,22 +61,7 @@ function handleError(error) {
     document.getElementById("location").innerHTML = error.message;
 }
 
-//住所を表示する
-function showAddress(latitude, longitude) {
-    try {
-        var gclient = new google.maps.ClientGeocoder();
-        var pos = new google.maps.LatLng(latitude, longitude);
-        //Google Map API を使って住所を取得
-        gclient.getLocations(pos, function(response) {
-            if (response && response.Status.code == 200) {
-                document.getElementById("address").innerHTML = response.Placemark[0].address;
-            }
-        });
-    }
-    catch (e) {
-        alert(e.message);
-    }
-}
-
 google.setOnLoadCallback(initialize);
+
+
 
