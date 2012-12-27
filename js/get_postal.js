2ea26669e5c66613cd120153f11d18403af7c32f
Yuki_local <t12968yy@sfc.keio.ac.jp>
2012-12-27 16:21:18 +0900

    GPS 2 nearest station has been 実装ed

---
+++ js/get_postal.js
@@ -0,0 +1,52 @@
+google.load("maps", "2.x");
+
+// 現在地表示
+function initialize() {
+    if (navigator.geolocation) {
+        //Geolocation API を使った現在地の取得
+        navigator.geolocation.getCurrentPosition(showPosition, handleError);
+    }
+}
+
+//現在地の位置情報を表示する
+//あとAPIを用いて付近の駅を検索する。
+function showPosition(position) {
+    document.getElementById("location").innerHTML = "(" + position.coords.latitude + ", " + position.coords.longitude + ")";
+
+    postal(position.coords.longitude, position.coords.latitude);
+}
+
+function handleError(error) {
+    document.getElementById("location").innerHTML = error.message;
+}
+
+google.setOnLoadCallback(initialize);
+
+
+// 郵便番号変換
+function postal(x,y) {
+    var target = document.createElement('script');
+    target.charset = 'utf-8';
+    target.src = "http://express.heartrails.com/api/json"
+        + "?method=getStations"
+        + "&x=" + x
+        + "&y=" + y
+        + "&jsonp=callbackf";
+    document.body.appendChild(target);
+}
+function callbackf(data) {
+    var out = document.getElementById('out');
+    var code = document.getElementById('code');
+    out.innerHTML = "<table border=\"0\" align=\"left\" style=\"table-layout: fixed;\">";
+
+
+    for( var i = 0; i < data.response.station.length; i++ ) {
+	code.innerHTML +=  data.response.station[i].line + " " + data.response.station[i].name + " " + data.response.station[i].distance + "<br/>";
+
+	out.innerHTML = "<tr width=\"200\"><td>data.response.station[i].line</td><td>data.response.station[i].name</td><td>data.response.station[i].distance</td><tr/>";
+
+    }
+    out.innerHTML = "</table>";
+
+
+}
