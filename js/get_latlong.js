92fefcf889f6217a243579d29d1a45a59e9385c0
Yuki_local <t12968yy@sfc.keio.ac.jp>
2012-12-27 13:43:44 +0900

    GPS OK Train 2 ekitan OK Changed format of homepage

--- js/get_latlong.js
+++ js/get_latlong.js
@@ -1,43 +1,5 @@
 google.load("maps", "2.x");
 
-// 最寄り駅検索
-var zip_data = null;
-
-function search(post) {
-    var target = document.createElement('script');
-    target.charset = 'utf-8';
-    target.src = "http://geoapi.heartrails.com/api/json"
-        + "?method=getStations"
-        + "&postal=" + post
-        + "&jsonp=callbackf";
-    document.body.appendChild(target);
-}
-
-function callbackf(data) {
-    var out = document.getElementById('codes');
-    out.innerHTML += JSON.stringify(data);
-}
-
-// 郵便番号変換
-var zip_data = null;
-
-function postal(x,y) {
-    var target = document.createElement('script2');
-    target.charset = 'utf-8';
-    target.src = "http://geoapi.heartrails.com/api/json"
-        + "?method=searchByGeoLocation"
-        + "&x=" + x
-        + "&y=" + y
-        + "&jsonp=callbackf";
-    document.body.appendChild(target);
-}
-
-function callbackf(data) {
-    var out = document.getElementById('codes');
-    out.innerHTML += JSON.stringify(data);
-}
-
-
 // 現在地表示
 function initialize() {
     if (navigator.geolocation) {
