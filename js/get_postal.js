4ed1a9cf563865ecb349c93a26c20ea59f938fcd
Yuki_local <t12968yy@sfc.keio.ac.jp>
2013-01-13 16:10:51 +0900

    edited css modified latlong 	 住所表示 	 整形 indexもきれいに 	説明も入れた

--- js/get_postal.js
+++ js/get_postal.js
@@ -12,21 +12,34 @@ function initialize() {
 //現在地の位置情報を表示する
 //あとAPIを用いて付近の駅を検索する。
 function showPosition(position) {
-    document.getElementById("location").innerHTML = "(" + position.coords.latitude + ", " + position.coords.longitude + ")";
-
+    document.getElementById("location").innerHTML = "Latitude : " + position.coords.latitude + "<br/>Longitude : " + position.coords.longitude + "<br/>";
+    showAddress(position.coords.latitude,position.coords.longitude);
     postal(position.coords.longitude, position.coords.latitude);
-    var latlng = new google.maps.LatLng(position.coords.longitude, position.coords.latitude);
-    var opts = {
-	zoom: 13,
-	center: latlng,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),opts);
 }
 
 function handleError(error) {
     document.getElementById("location").innerHTML = error.message;
 }
+
+// 経度緯度->住所変換
+function showAddress(latitude, longitude) {
+    try {
+        var gclient = new google.maps.ClientGeocoder();
+        var pos = new google.maps.LatLng(latitude, longitude);
+
+        //Google Map API を使って住所を取得
+        gclient.getLocations(pos, function(response) {
+            if (response && response.Status.code == 200) {
+                document.getElementById('address').innerHTML = response.Placemark[0].address + "<br/>";
+            }
+        });
+    }
+    catch (e) {
+        alert(e.message);
+    }
+}
+
+
 
 google.setOnLoadCallback(initialize);
 
@@ -41,6 +54,16 @@ function postal(x,y) {
         + "&y=" + y
         + "&jsonp=callbackf";
     document.body.appendChild(target);
+
+    var latlng = new google.maps.LatLng(x,y);
+/*    var opts = {
+	zoom: 8,
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	center: latlng
+    };*/
+//    var map = new google.maps.Map(document.getElementById('map_canvas'),opts);
+    var map = new google.maps.Map(document.getElementById('map_canvas'));
+
 }
 function callbackf(data) {
     var out = document.getElementById('out');
@@ -49,7 +72,8 @@ function callbackf(data) {
 // Tableとして整形するため
 //    out.innerHTML = "<table border=\"0\" align=\"left\" style=\"table-layout: fixed;\">";
     for( var i = 0; i < data.response.station.length; i++ ) {
-	code.innerHTML +=  data.response.station[i].line + " " + data.response.station[i].name + " " + data.response.station[i].distance + "<br/>";
+	code.innerHTML +=  data.response.station[i].line + "<a href=\"http://timetable.ekitan.com/train/TimeSearch?SFNAME=" + data.response.station[i].name + "&SFCODE=&SFList=%89w%88%EA%97%97&btn_search.x=0&btn_search.y=0\">" + "   " + data.response.station[i].name + "</a>" + "   " + data.response.station[i].distance + "<br/>";
+
 
 // Tableとして整形するため
 //	out.innerHTML = "<tr width=\"200\"><td>data.response.station[i].line</td><td>data.response.station[i].name</td><td>data.response.station[i].distance</td><tr/>";
@@ -57,6 +81,9 @@ function callbackf(data) {
     }
 // Tableとして整形するため
 //    out.innerHTML = "</table>";
+
+
+
 }
 
 
